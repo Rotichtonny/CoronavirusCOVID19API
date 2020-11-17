77f3fa9f379535e199e6f7dc5abae72539a98656
Rotichtonny <tonnyrotich.tr@gmail.com>
2020-11-17 22:32:43 +0300

    Modified Web API Routes

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -22,7 +22,7 @@ namespace CoronavirusCOVID19
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
